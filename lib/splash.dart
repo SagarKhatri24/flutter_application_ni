6cdf56b13d9fc45d3c2d04dae35bef49cf908b3d
Sagar Khatri <khatrisagar2@gmail.com>
2025-09-30 15:50:53 +0530

    Payment Gateway

--- lib/splash.dart
+++ lib/splash.dart
@@ -9,6 +9,7 @@ import 'package:flutter_application_ni/jsonSignup.dart';
 import 'package:flutter_application_ni/login.dart';
 import 'package:flutter_application_ni/navigationDrawer.dart';
 import 'package:flutter_application_ni/profile.dart';
+import 'package:flutter_application_ni/razorpayDemo.dart';
 import 'package:flutter_application_ni/signup.dart';
 import 'package:flutter_application_ni/tabDemo.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -47,14 +48,15 @@ class SplashDemo extends State<SplashApp>{
     var sUserId = sp.getString(ConstantData.USERID) ?? "";
     
     return new Timer(Duration(seconds: 3), (){
-      //Navigator.push(context, MaterialPageRoute(builder: (_)=> JsonLoginApp() ));
-      if(sUserId == ""){
-        //Navigator.push(context, MaterialPageRoute(builder: (_)=> TabApp() ));
-        Navigator.push(context, MaterialPageRoute(builder: (_)=> JsonLoginApp() ));
-      }
-      else{
-        Navigator.push(context, MaterialPageRoute(builder: (_)=> JsonProfileApp() ));        
-      }
+      Navigator.push(context, MaterialPageRoute(builder: (_)=> RazorpayPage() ));
+      
+      // if(sUserId == ""){
+      //   //Navigator.push(context, MaterialPageRoute(builder: (_)=> TabApp() ));
+      //   Navigator.push(context, MaterialPageRoute(builder: (_)=> JsonLoginApp() ));
+      // }
+      // else{
+      //   Navigator.push(context, MaterialPageRoute(builder: (_)=> JsonProfileApp() ));        
+      // }
     });
   }
 
